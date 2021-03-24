3f62d4905b92a32e09a500b69789559330bf43f7
Md. Iqbal Hossain <iqbal.hossain@sslwireless.com>
2021-03-24 23:56:27 +0600

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+
+
+// public routes
+Route::post('/login', 'Auth\ApiAuthController@login')->name('login.api');
+Route::post('/register','Auth\ApiAuthController@register')->name('register.api');
+
+
+
+Route::middleware('auth:api')->group(function () {
+    // our routes to be protected will go in here
+    Route::post('/logout', 'Auth\ApiAuthController@logout')->name('logout.api');
+    Route::apiResource('users', UserController::class);
+});
